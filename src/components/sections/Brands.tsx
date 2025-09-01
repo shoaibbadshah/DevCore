37d3848610712822e3c2d8dfd451896440929b17
Shoaib Badshah <eng.shoaib.badshah@gmail.com>
2025-09-01 11:40:13 +0000

    these should scroll

--- src/components/sections/Brands.tsx
+++ src/components/sections/Brands.tsx
@@ -1,29 +1,32 @@
 import Image from 'next/image';
 
 const brandLogos = [
-  { src: '/img/brand/01.png', alt: 'Brand 1' },
-  { src: '/img/brand/02.png', alt: 'Brand 2' },
-  { src: '/img/brand/03.png', alt: 'Brand 3' },
-  { src: '/img/brand/04.png', alt: 'Brand 4' },
-  { src: '/img/brand/05.png', alt: 'Brand 5' },
-  { src: '/img/brand/06.png', alt: 'Brand 6' },
+  { src: '/img/brand/01.png', alt: 'Reliance Insurance Group' },
+  { src: '/img/brand/02.png', alt: 'Holdings' },
+  { src: '/img/brand/03.png', alt: 'Mobile Tech' },
+  { src: '/img/brand/04.png', alt: 'Western' },
+  { src: '/img/brand/05.png', alt: 'AudioWave' },
+  { src: '/img/brand/06.png', alt: 'Pandora' },
 ];
 
 export default function Brands() {
   return (
-    <section className="py-8 bg-secondary">
+    <section className="py-12 bg-secondary">
       <div className="container mx-auto px-4 md:px-6">
-        <div className="flex justify-around items-center gap-8 flex-wrap">
-          {brandLogos.map((brand, index) => (
-            <Image
-              key={index}
-              src={brand.src}
-              alt={brand.alt}
-              width={150}
-              height={50}
-              className="object-contain"
-            />
-          ))}
+        <div className="relative overflow-hidden group">
+          <div className="flex animate-marquee group-hover:pause">
+            {[...brandLogos, ...brandLogos].map((brand, index) => (
+              <div key={index} className="flex-shrink-0 w-1/6 flex justify-center items-center mx-8">
+                <Image
+                  src={brand.src}
+                  alt={brand.alt}
+                  width={150}
+                  height={50}
+                  className="object-contain"
+                />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </section>
